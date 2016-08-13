db80e4c52221fb2891329ee876b7d5bfa34f765c
roberrera <roberterrera@gmail.com>
2016-08-13 19:06:44 -0400

    Cleanup some code

---
+++ app/src/main/java/com/roberterrera/programmersbylocale/model/adapters/PeopleViewAdapter.java
@@ -0,0 +1,7 @@
+package com.roberterrera.programmersbylocale.model.adapters;
+
+/**
+ * Created by Rob on 8/13/16.
+ */
+public class PeopleViewAdapter {
+}
--- app/src/main/java/com/roberterrera/programmersbylocale/view/PeopleViewActivity.java
+++ app/src/main/java/com/roberterrera/programmersbylocale/view/PeopleViewActivity.java
@@ -1,11 +1,8 @@
 package com.roberterrera.programmersbylocale.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.roberterrera.programmersbylocale.R;
 
@@ -22,14 +19,6 @@ public class PeopleViewActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setTitle("People");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
