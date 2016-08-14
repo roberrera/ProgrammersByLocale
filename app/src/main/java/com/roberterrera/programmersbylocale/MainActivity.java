75403479fb9c009069c3140d38b1211bdbfb682c
roberrera <roberterrera@gmail.com>
2016-08-14 10:25:21 -0400

    Extend ActivityMethods.class from MainActivity and clean up MainActivity

--- app/src/main/java/com/roberterrera/programmersbylocale/MainActivity.java
+++ app/src/main/java/com/roberterrera/programmersbylocale/MainActivity.java
@@ -1,26 +1,15 @@
 package com.roberterrera.programmersbylocale;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
-import com.google.gson.Gson;
-import com.roberterrera.programmersbylocale.model.JSONResponse;
-import com.roberterrera.programmersbylocale.model.ProgrammerLocation;
 import com.roberterrera.programmersbylocale.model.adapters.LocaleViewAdapter;
 
 import org.json.JSONException;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
-    private List<String> localesList;
+public class MainActivity extends ActivityMethods {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,71 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Select a locale");
 
+        List<String> localesList = new ArrayList<>();
+        LocaleViewAdapter adapter = new LocaleViewAdapter(localesList, this);
+        String jsonFile = "android_model_challenge.json";
+
         /* Load list of localities from JSON data */
-        localesList = new ArrayList<>();
         try {
-            loadLocales(localesList);
+            loadLocales(localesList, jsonFile);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        setUpRecyclerView();
-    }
-
-    public void setUpRecyclerView() {
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview_list);
-
-        LocaleViewAdapter mAdapter = new LocaleViewAdapter(localesList, MainActivity.this);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-
-        if (localesList.size() > 0) {
-            if (recyclerView != null) {
-
-                recyclerView.setHasFixedSize(true);
-                recyclerView.setLayoutManager(layoutManager);
-                recyclerView.setAdapter(mAdapter);
-
-            }
-        }
-    }
-
-    public void loadLocales(List<String> locales) throws JSONException {
-        Gson gson = new Gson();
-        String jsonFile = loadJSONFromAsset();
-
-        try {
-            JSONResponse response = gson.fromJson(jsonFile,
-                    JSONResponse.class);
-            List<ProgrammerLocation> result = response.getResponse().getLocations();
-
-        for (int i = 0; i < result.size(); i++){
-            String locale = response.getResponse().getLocations().get(i).getLocality();
-                locales.add(locale);
-                Log.d("Locale: ", String.valueOf(locale));
-            }
-        } catch (IllegalStateException e){
-            e.printStackTrace();
-        }
-        Log.d("Locales: ", String.valueOf(locales));
-    }
-
-
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = getAssets().open("android_model_challenge.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, "UTF-8");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        System.out.println(json);
-        return json;
+        setUpLocaleRecyclerView(localesList, adapter);
     }
 }
